c841f9c1a5f3d31330fe9a8961d2a7be9cbdb9ed
DinoMax00 <527663226@qq.com>
2022-06-22 20:47:57 +0800

    目录结构

--- db/netdisk.sql
+++ db/netdisk.sql
@@ -1,3 +1,43 @@
 drop database if exists netdisk;
 create database netdisk;
 use netdisk;
+
+# 用户表
+create table user (
+  id int not null auto_increment,
+  username varchar(20) not null,
+  password varchar(20) not null,
+  primary key (id)
+);
+
+# 文件表
+drop table if exists files;
+create table files (
+  id int primary key auto_increment,
+  name varchar(255) not null,
+  size int not null,
+  path varchar(255) not null,
+  link_count int not null,
+  create_time datetime not null,
+  update_time datetime not null,
+);
+
+
+# 目录表
+drop table if exists directories;
+create table directories (
+  id int primary key auto_increment,
+  name varchar(255) not null,
+  par int not null,
+  create_time datetime not null,
+  update_time datetime not null,
+);
+
+
+# 联系表
+drop table if exists contacts;
+create table contacts (
+  id int primary key auto_increment,
+  file int not null,
+  directory int not null,
+);
